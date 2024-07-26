b5fe821238400807fc07371ae809728abbc2bd1e
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-07-27 00:34:17 +0100

    update script.js

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const result = document.getElementById('results-div')
  
-const validationFunction = () => {
-    const numberPattern = /^(?:\+1\s?)?(\(\d{3}\)\s?|\d{3}-?)\d{3}-?\d{4}$/;
-    if (userInput.value === numberPattern) {
-        result.innerText = `Valid US number: ${userInput}`
+const validationFunction = () => {   
+    const numberPattern =  /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
+    if (numberPattern.test(userInput.value)) {
+        result.innerText = `Valid US number: ${userInput.value}`
     } else {
         result.innerText = `Invalid US number: ${userInput.value}`
     }
 }
 
 
-
 checkBtn.addEventListener('click', () => {
     if (!userInput.value) {
         alert("Please provide a phone number")
-        result.innerText = ''
+        return
     } else {
         validationFunction()
     }
